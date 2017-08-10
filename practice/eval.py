c2866b3187d54563484f98be879b4a2478b2cb84
Song Wei <1983.song.wei@gmail.com>
2017-08-10 15:47:06 -0400

    t

--- practice/eval.py
+++ practice/eval.py
@@ -13,8 +13,7 @@ def fini(coord, threads, filename_queue):
     coord.request_stop()
     coord.join(threads)
 
-with tf.Session() as sess:
-    input = tf.random_normal([])
+def build_shuffle_batch_test():
     BATCH_SIZE = 2
     min_after_dequeue = BATCH_SIZE * 2
     capacity = min_after_dequeue + 5 * BATCH_SIZE
@@ -24,8 +23,13 @@ with tf.Session() as sess:
         batch_size=BATCH_SIZE,
         capacity=capacity,
         min_after_dequeue=min_after_dequeue)
+    return shuffled_batch
 
-    print shuffled_batch
+
+with tf.Session() as sess:
+    input = tf.random_normal([])
+    shuffled_batch = build_shuffle_batch_test()
+
     try:
         coord, threads = init()
         print sess.run([shuffled_batch])
